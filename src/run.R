6f2829d805dda23a0ebd099cd0b7853c99cb3bd7
Tien-Cheng <39258210+Illustratien@users.noreply.github.com>
2025-05-15 21:11:03 +0200

    add installed packages check

--- src/run.R
+++ src/run.R
@@ -1,4 +1,21 @@
 rm(list=ls())
+packages_to_check <- c('dplyr','ggplot2','magick','ggcorrplot','toolPhD','tibble',
+                       'scales','ggh4x','rlang','gridExtra','conflicted','agricolae',
+                       'tidyr','ggpmisc','parallel','doParallel',
+                       'pacman','plyr','purrr','knitr','toolStability','cowplot','ggrepel','corrplot')
+check_and_install <- function(package_name) {
+  if (!requireNamespace(package_name, quietly = TRUE)) {
+    install.packages(package_name, repos = "https://cran.r-project.org")
+    #  requireNamespace(package_name, quietly = TRUE) # No need to load here, the user will load.
+  } else {
+    message(paste(package_name, "is already installed.")) # Inform the user that the package was already installed
+  }
+}
+
+# Use lapply to apply the function to each package in the vector
+lapply(packages_to_check, check_and_install)
+
+
 library(dplyr)
 conflicted::conflict_prefer("filter", "dplyr") %>% suppressMessages()
 conflicted::conflict_prefer("select", "dplyr") %>% suppressMessages()
